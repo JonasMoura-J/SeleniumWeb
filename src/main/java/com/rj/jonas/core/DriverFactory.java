12a937fb9580f78d98b2297c44c3e0a409d6215d
JonasMoura-J <jonasmourat9090@gmail.com>
2021-04-17 11:48:20 -0300

    Adicionando configuração de um driver por thread

--- src/main/java/com/rj/jonas/core/DriverFactory.java
+++ src/main/java/com/rj/jonas/core/DriverFactory.java
@@ -6,27 +6,40 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class DriverFactory {
 	
-	private static WebDriver driver;
+	//private static WebDriver driver;
+	private static ThreadLocal<WebDriver> threadDriver= new ThreadLocal<WebDriver>() {
+		@Override
+		protected synchronized WebDriver initialValue() {
+			return initDriver();
+		}
+	};
 	
 	private DriverFactory() {}
 	
-	public static WebDriver getDriver(){
-		if(driver == null) {
-			switch (Propriedades.browser) {
-			case FIREFOX: driver = new FirefoxDriver();
-				break;
-			case CHROME: driver = new ChromeDriver();
-				break;
-			}
-			driver = new ChromeDriver();
+	public static WebDriver getDriver() {
+		return threadDriver.get();
+	}
+	
+	public static WebDriver initDriver(){
+		WebDriver driver = null;
+		
+		switch (Propriedades.browser) {
+		case FIREFOX: driver = new FirefoxDriver();
+			break;
+		case CHROME: driver = new ChromeDriver();
+			break;
 		}
 		return driver;
 	}
 
 	public static void killDriver(){
+		WebDriver driver = getDriver();
 		if(driver != null) {
 			driver.quit();
 			driver = null;
+		}
+		if(threadDriver != null) {
+			threadDriver.remove();
 		}
 	}
 }
--- src/main/java/com/rj/jonas/suites/SuiteGeral.java
+++ src/main/java/com/rj/jonas/suites/SuiteGeral.java
@@ -1,13 +1,9 @@
 package com.rj.jonas.suites;
 
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
-import com.rj.jonas.core.DriverFactory;
-import com.rj.jonas.pages.LoginPage;
 import com.rj.jonas.tests.ContaTest;
 import com.rj.jonas.tests.MovimentacaoTest;
 import com.rj.jonas.tests.RemoverMovimentacaoContaTest;
@@ -23,19 +19,5 @@ import com.rj.jonas.tests.SaldoTest;
 	ResumoTest.class
 })
 public class SuiteGeral {
-	private static LoginPage page = new LoginPage();
 	
-	@BeforeClass
-	public static void inicializa(){
-		page.acessarTelaInicial();
-		
-		page.setEmail("wagner@costa");
-		page.setSenha("123456");
-		page.entrar();
-	}
-	
-	@AfterClass
-	public static void finaliza(){
-		DriverFactory.killDriver();
-	}
 }
